47156f0c7bc7627633b967bb23507f9e8ab415cb
HyeRiJeong <cos850@naver.com>
2024-09-18 21:27:02 +0900

    test: calculator junit 테스트 추가

---
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -0,0 +1,72 @@
+package com.group.libraryapp.calculator
+
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+
+class JunitCalculatorTest {
+
+    @Test
+    fun plusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.plus(3)
+
+        // then
+        // assertProvider<T> 인자를 받는 함수를 import로 추가
+        assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minusTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiplyTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when & then
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(2)
+        }.apply {
+            assertThat(message).isEqualTo("0으로 나눌 수 없습니다")
+        }
+    }
+
+}
